7f92b193d52dbffe1f6564b39a4abcd78c685912
Morgan Thomas <m@m0rg.dev>
2020-12-16 10:31:02 -0800

    make rebuild.sh handle packages with no buildrequires

--- rebuild.sh
+++ rebuild.sh
@@ -6,6 +6,7 @@ set -x
 XID=$(dnf history info | head -n1 | cut -d ':' -f 2)
 SPEC=$1
 
-dnf install -y $(rpmspec -q --buildrequires $SPEC)
+BUILDREQUIRES=$(rpmspec -q --buildrequires $SPEC)
+[ -n "$BUILDREQUIRES" ] && dnf install -y 
 rpmbuild -ba $@
 dnf history rollback -y $XID
