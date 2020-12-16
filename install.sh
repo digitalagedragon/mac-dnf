2aee3bcbbf24b807f1b8e2d4c50a45ee7ed8db40
Morgan Thomas <m@m0rg.dev>
2020-12-16 11:22:23 -0800

    inline setup_usrlocal.sh into install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,11 @@ echo "### Setting up /usr/local... (requires sudo)"
 echo "This will create a directory structure under /usr/local and reassign its ownership."
 echo "If you have Homebrew installed, this is a good time to kill the script and move or uninstall it."
 
-sh setup_usrlocal.sh
+my_uid=$(id -u)
+my_gid=$(id -g)
+
+sudo mkdir -p /usr/local/{bin,etc,include,lib,libexec,opt,sbin,share,var}
+sudo chown $my_uid:$my_gid /usr/local/{bin,etc,include,lib,libexec,opt,sbin,share,var}
 
 echo "### Downloading bootstrap packages..."
 cd /tmp
--- setup_usrlocal.sh
+++
@@ -1,5 +0,0 @@
-my_uid=$(id -u)
-my_gid=$(id -g)
-
-sudo mkdir -p /usr/local/{bin,etc,include,lib,libexec,opt,sbin,share,var}
-sudo chown $my_uid:$my_gid /usr/local/{bin,etc,include,lib,libexec,opt,sbin,share,var}
