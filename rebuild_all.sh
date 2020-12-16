04b8f1eb4001ffaf638e0ca042637bf209a8fdc6
Morgan Thomas <m@m0rg.dev>
2020-12-16 10:55:14 -0800

    make rebuild.sh recreate the local repo after building something

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -7,12 +7,26 @@ cd $(dirname $0)
 
 for pkg in $(cat packages.order); do
     [ -e rpmbuild/SRPMS/"$(/usr/local/bin/rpmspec -q --srpm rpmbuild/SPECS/$pkg.spec | sed -E 's|\.[^.]*$||')".src.rpm ] \
-        || ./rebuild.sh rpmbuild/SPECS/$pkg.spec
+        || {
+            ./rebuild.sh rpmbuild/SPECS/$pkg.spec
+            cd repo
+            rm -rf *
+            cp -r ../rpmbuild/RPMS/* .
+            createrepo_c .
+            dnf makecache
+        }
 done
 
 if [ $(uname -m) = "arm64" ]; then
     for pkg in $(cat packages.universal); do
         [ -e rpmbuild/SRPMS/"$(/usr/local/bin/rpmspec -q --srpm rpmbuild/SPECS/$pkg.spec --with universal | sed -E 's|\.[^.]*$||')".src.rpm ] \
-            || ./rebuild.sh rpmbuild/SPECS/$pkg.spec --with universal
+            || {
+                ./rebuild.sh rpmbuild/SPECS/$pkg.spec --with universal
+                cd repo
+                rm -rf *
+                cp -r ../rpmbuild/RPMS/* .
+                createrepo_c .
+                dnf makecache
+            }
     done
 fi
