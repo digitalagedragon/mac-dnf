ef8da38b7c041f28b6600e89d600a3817f1f2066
Morgan Thomas <m@m0rg.dev>
2020-12-16 14:28:22 -0800

    make rebuild.sh rollback the DNF transaction even on build failure

--- rebuild.sh
+++ rebuild.sh
@@ -4,9 +4,9 @@ set -e
 set -x
 
 XID=$(dnf history info | head -n1 | cut -d ':' -f 2)
+trap dnf history rollback -y $XID EXIT
 SPEC=$1
 
 BUILDREQUIRES=$(rpmspec -q --buildrequires $SPEC)
 [ -n "$BUILDREQUIRES" ] && dnf install -y $BUILDREQUIRES
 rpmbuild -ba $@
-dnf history rollback -y $XID
