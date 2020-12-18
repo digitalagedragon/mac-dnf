1d7fb8e0a10412922e5959c9832befcfaea86d6d
Morgan Thomas <m@m0rg.dev>
2020-12-17 21:13:51 -0800

    adjust permissions on /usr/local subdirs in install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,9 @@ echo "### Setting up /usr/local... (requires sudo)"
 echo "This will create a directory structure under /usr/local and reassign its ownership."
 echo "If you have Homebrew installed, this is a good time to kill the script and move or uninstall it."
 
-my_uid=$(id -u)
-my_gid=$(id -g)
-
 sudo mkdir -p /usr/local/{bin,etc,include,lib,libexec,opt,sbin,share,var}
-sudo chown $my_uid:$my_gid /usr/local/{bin,etc,include,lib,libexec,opt,sbin,share,var}
+sudo chown :admin /usr/local/{bin,etc,include,lib,libexec,opt,sbin,share,var}
+sudo chmod 775 /usr/local/{bin,etc,include,lib,libexec,opt,sbin,share,var}
 
 echo "### Downloading bootstrap packages..."
 cd /tmp
